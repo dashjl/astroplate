ac0539aa20d92223de01f766c1b9323073cfb676
dashjl <14898258+dashjl@users.noreply.github.com>
2025-12-18 20:08:39 -0800

    Get rid of content collections with one page

--- src/content.config.ts
+++ src/content.config.ts
@@ -46,106 +46,19 @@ const authorsCollection = defineCollection({
   }),
 });
 
-// Pages collection schema
-const pagesCollection = defineCollection({
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/pages" }),
-  schema: z.object({
-    ...commonFields,
-  }),
-});
-
-// about collection schema
-const aboutCollection = defineCollection({
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/about" }),
-  schema: z.object({
-    ...commonFields,
-  }),
-});
 
 
 
-const bannerSchema = z.object({
-  title: z.string(),
-  content: z.string(),
-  image: z.string(),
-  button: z.object({
-    enable: z.boolean(),
-    label: z.string(),
-    link: z.string(),
-  }),
-});
 
-// Homepage collection schema
-const homepageCollection = defineCollection({
-  loader: glob({ pattern: "**/-*.{md,mdx}", base: "src/content/homepage" }),
-  schema: z.object({
-    banner: bannerSchema.optional(),
-    features: z.array(
-      z.object({
-        title: z.string(),
-        image: z.string(),
-        content: z.string(),
-        bulletpoints: z.array(z.string()),
-        button: z.object({
-          enable: z.boolean(),
-          label: z.string(),
-          link: z.string(),
-        }),
-      }),
-    ).optional(),
-  }),
-});
 
-// Call to Action collection schema
-const ctaSectionCollection = defineCollection({
-  loader: glob({
-    pattern: "call-to-action.{md,mdx}",
-    base: "src/content/sections",
-  }),
-  schema: z.object({
-    enable: z.boolean(),
-    title: z.string(),
-    description: z.string(),
-    image: z.string(),
-    button: z.object({
-      enable: z.boolean(),
-      label: z.string(),
-      link: z.string(),
-    }),
-  }),
-});
 
-// Testimonials Section collection schema
-const testimonialSectionCollection = defineCollection({
-  loader: glob({
-    pattern: "testimonial.{md,mdx}",
-    base: "src/content/sections",
-  }),
-  schema: z.object({
-    enable: z.boolean(),
-    title: z.string(),
-    description: z.string(),
-    testimonials: z.array(
-      z.object({
-        name: z.string(),
-        avatar: z.string(),
-        designation: z.string(),
-        content: z.string(),
-      }),
-    ),
-  }),
-});
+
+
+
 
 // Export collections
 export const collections = {
   // Pages
-  homepage: homepageCollection,
   blog: blogCollection,
   authors: authorsCollection,
-  pages: pagesCollection,
-  about: aboutCollection,
-
-  // sections
-  ctaSection: ctaSectionCollection,
-  testimonialSection: testimonialSectionCollection,
 };
